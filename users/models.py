e7f89b38bbce634f15e08525ee3569361489b228
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-06-19 17:43:30 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class CustomUser(AbstractUser):
         return self.username
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
