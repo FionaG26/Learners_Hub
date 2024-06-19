8792d79e132986eb28aa3aa3f9aca3757ac9ad44
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-06-19 18:46:20 +0300

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.course_list, name='course_list'),
     path('<int:pk>/', views.course_detail, name='course_detail'),
     path('create/', views.course_create, name='course_create'),
-    path('resources/online-courses/', views.online_courses, name='online_courses'),
     path('resources/books/', views.books, name='books'),
     path('resources/blogs/', views.blogs, name='blogs'),
 ]
