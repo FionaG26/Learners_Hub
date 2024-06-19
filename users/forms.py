a3c7b1c877d87da95c4cefbaeaa30312503b647b
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-06-19 17:53:14 +0300

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
+# users/forms.py
+
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser, profile
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser, Profile  # Correct the import here
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -9,7 +11,7 @@ class CustomUserCreationForm(UserCreationForm):
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
-        model = Profile
+        model = Profile  # Use Profile instead of profile
         fields = ['bio', 'location', 'birth_date']
 
 class CustomUserChangeForm(UserChangeForm):
