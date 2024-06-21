31da889d4f198c129a73af1cb0ff259e5d9e23e2
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-06-21 18:25:07 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+    path('login/', views.login_view, name='login'),  # Correct, used once
     path('logout/', views.logout_view, name='logout'),
     path('home/', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),path('login/', views.login_view, name='login'),
+    path('contact/', views.contact, name='contact'),
     path('subscribe/', views.subscribe, name='subscribe'),
     path('profile/update/', views.update_profile, name='update_profile'),
 ]
